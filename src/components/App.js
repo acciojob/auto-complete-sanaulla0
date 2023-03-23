3860106ac5126f7456ef036581e42e2e117c2875
sanaulla0 <vadapallisanaulla@gmail.com>
2023-03-23 19:40:11 +0530

    p1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
 
-import React from "react";
+import React ,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [data , setData] = useState([]);
+  const [store,setStore] = useState([]);
+ const  fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+const hadelChange = (e)=>{
+             setData(e.target.value);
+             console.log(data);
+
+}
+const filteredfruits= fruits.filter((item)=>(
+     item.includes(data)
+));
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Search items</h1>
+<input type="text" onChange={hadelChange}  />
+   <div>
+     <ul>
+             {filteredfruits.map((item)=>(
+                   <li key={item}>{item}</li>
+             ))}
+             
+                     
+            
+        
+     </ul>
+   </div>
     </div>
   )
 }
 
-export default App
+export default App; 
